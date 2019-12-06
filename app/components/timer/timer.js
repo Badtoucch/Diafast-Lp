f8eb78f65c6dc2989a9dac9b9a5c16c90971b048
Yulia Bondar <51965711+BondarYulia@users.noreply.github.com>
2019-12-06 12:58:50 +0200

    Yulia pr2

--- app/components/timer/timer.js
+++ app/components/timer/timer.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
-    $(".timer").fadeOut();
-    
+
     $(window).on("scroll", function () {
        
         if (($(window).scrollTop() > 800) && ($(window).scrollTop() < 10500)) {
             $(".timer").fadeIn();
+            
         } else {
             $(".timer").fadeOut();
+            
         }
 
     });
